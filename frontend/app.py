cb971d3f93a1e22603722e5d5be4d10aeee562e0
Hrishikesh Laxman Dherange <hrishikeshdh@cybage.com>
2026-08-19 16:19:03 +0530

    update the service name for front end to call backend svc

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    backend_url = "http://localhost:5001"
+    backend_url = os.getenv("BACKEND_URL", "http://localhost:5001")
     return render_template("index.html", backend_url=backend_url)
 
 if __name__ == "__main__":
